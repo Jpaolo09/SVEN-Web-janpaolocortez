50f21c4aa9c3d818b69e15edbbc23b2fab56993c
Jpaolo09 <jp.cortez091@gmail.com>
2025-04-14 17:10:16 +0800

    Intial setup for migration, model, controlle, route, and view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('appointments', AppointmentController::class)->only(['create', 'store']);
